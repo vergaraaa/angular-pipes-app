e8500fa5f4056be4bb9513a829018c00132da550
Edgar Vergara <edgarvm13@hotmail.com>
2024-08-13 16:19:56 +0200

    ✨ navigation in menu

--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -18,14 +18,17 @@ export class MenuComponent implements OnInit {
           {
             label: 'Texts and Date',
             icon: 'pi pi-align-left',
+            routerLink: '/',
           },
           {
             label: 'Numbers',
             icon: 'pi pi-dollar',
+            routerLink: 'numbers',
           },
           {
             label: 'Uncommon',
             icon: 'pi pi-globe',
+            routerLink: 'uncommon',
           },
         ],
       },
