22b79e77f58a8a617128acffb883f3bf7f087a49
Edgar Vergara <edgarvm13@hotmail.com>
2024-08-13 19:44:28 +0200

    ✨ numeric pipes

--- src/app/products/pages/numbers-page/numbers-page.component.ts
+++ src/app/products/pages/numbers-page/numbers-page.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-numbers-page',
   templateUrl: './numbers-page.component.html',
-  styleUrl: './numbers-page.component.css'
+  styleUrl: './numbers-page.component.css',
 })
 export class NumbersPageComponent {
-
+  public totalSells: number = 13769485.329;
+  public percent: number = 0.3413;
 }
