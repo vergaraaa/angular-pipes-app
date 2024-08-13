4784c796e72eb9810d01f131018571e7a1ff5d16
Edgar Vergara <edgarvm13@hotmail.com>
2024-08-13 19:33:21 +0200

    ✨ locales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -7,6 +7,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { PrimeNgModule } from './prime-ng/prime-ng.module';
 import { SharedModule } from './shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// locale config
+import localeEsMX from '@angular/common/locales/es-MX';
+import localeIT from '@angular/common/locales/it';
+
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeEsMX);
+registerLocaleData(localeIT);
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,7 +26,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     PrimeNgModule,
     SharedModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'en-US',
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
