dc9d4a90872fda6fa7eb1bc44e01fbe975199587
Edgar Vergara <edgarvm13@hotmail.com>
2024-08-14 11:00:40 +0200

    ✨ sort by pipe

--- src/app/products/pages/order/order.component.ts
+++ src/app/products/pages/order/order.component.ts
@@ -8,6 +8,7 @@ import { Color, Hero } from '../../interfaces/hero.interface';
 })
 export class OrderComponent {
   public isUpperCase: boolean = false;
+  public sortBy?: keyof Hero;
 
   public heroes: Hero[] = [
     {
@@ -39,5 +40,9 @@ export class OrderComponent {
 
   toggleUpperCase(): void {
     this.isUpperCase = !this.isUpperCase;
+  }
+
+  changeOrder(value: keyof Hero) {
+    this.sortBy = value;
   }
 }
