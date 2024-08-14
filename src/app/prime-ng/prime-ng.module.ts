c8018d0463b980a687121c8216e994d95d62e34f
Edgar Vergara <edgarvm13@hotmail.com>
2024-08-14 10:22:14 +0200

    💄 heroes table

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -2,18 +2,20 @@ import { NgModule } from '@angular/core';
 
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { FieldsetModule } from 'primeng/fieldset';
 import { MenubarModule } from 'primeng/menubar';
 import { PanelModule } from 'primeng/panel';
-import { FieldsetModule } from 'primeng/fieldset';
+import { TableModule } from 'primeng/table';
 import { ToolbarModule } from 'primeng/toolbar';
 
 @NgModule({
   exports: [
     ButtonModule,
     CardModule,
+    FieldsetModule,
     MenubarModule,
     PanelModule,
-    FieldsetModule,
+    TableModule,
     ToolbarModule,
   ],
 })
